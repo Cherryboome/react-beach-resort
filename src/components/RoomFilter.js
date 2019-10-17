70d763987f50e487fd2b006a37f45d0e69bc7909
Cherryboome <muhamidifarian@yahoo.com>
2019-10-17 17:18:50 +0300

    created RoomContainer, RoomFilter and RoomList components

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomFilter = () => {
+  return <div>This component will filter all rooms.</div>;
+};
+
+export default RoomFilter;
---
+++ src/components/RoomList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomList = () => {
+  return <div>This is a listing off all availabe rooms.</div>;
+};
+
+export default RoomList;
